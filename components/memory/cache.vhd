f160332ef285f211234bd19c96ab89d2ad01b352
OmarBazaraa <omar_bazaraa@hotmail.com>
2018-04-13 14:56:53 +0200

    Fix and edit cache

--- components/memory/cache.vhd
+++ components/memory/cache.vhd
@@ -1,7 +1,6 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
-USE WORK.utilities.ALL;
 
 ENTITY cache IS
     PORT(
@@ -10,7 +9,9 @@ ENTITY cache IS
         WindowWR        : IN  STD_LOGIC;
         ResultWR        : IN  STD_LOGIC;
         Din             : IN  STD_LOGIC_VECTOR(39 DOWNTO 0);
-        Result          : OUT STD_LOGIC_VECTOR( 7 DOWNTO 0)
+
+        ResultDin       : IN  STD_LOGIC_VECTOR( 7 DOWNTO 0);
+        ResultDout      : OUT STD_LOGIC_VECTOR( 7 DOWNTO 0)
     );
 END ENTITY;
 
@@ -25,23 +26,23 @@ BEGIN
     BEGIN
         IF RISING_EDGE(CLK) THEN
             IF FilterWR='1' THEN
-                filter(0) <= window(1);
-                filter(1) <= window(2);
-                filter(2) <= window(3);
-                filter(3) <= window(4);
-                filter(4) <= Din;
+                filter(0)   <= filter(1);
+                filter(1)   <= filter(2);
+                filter(2)   <= filter(3);
+                filter(3)   <= filter(4);
+                filter(4)   <= Din;
             END IF;
 
             IF WindowWR='1' THEN
-                window(0) <= window(1);
-                window(1) <= window(2);
-                window(2) <= window(3);
-                window(3) <= window(4);
-                window(4) <= Din;
+                window(0)   <= window(1);
+                window(1)   <= window(2);
+                window(2)   <= window(3);
+                window(3)   <= window(4);
+                window(4)   <= Din;
             END IF;
 
             IF ResultWR='1' THEN
-                Result <= Din(7 DOWNTO 0);
+                ResultDout  <= ResultDin;
             END IF;
         END IF;
     END PROCESS;
