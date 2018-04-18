acf2d743766ca28020b9df0c2609b3fca2d33d17
Samir55 <discere044@icloud.com>
2018-04-18 16:29:00 +0200

    Adding accelerator counter.

--- accelerator/accelerator.vhd
+++ accelerator/accelerator.vhd
@@ -18,7 +18,28 @@ ENTITY accelerator IS
 END ENTITY;
 
 ARCHITECTURE arch_accelerator OF accelerator IS
+    --
+    -- Counter Signals
+    --
+    SIGNAL CounterCLK             	: STD_LOGIC;
+    SIGNAL CounterRST             	: STD_LOGIC;
+    SIGNAL CounterOut			: STD_LOGIC_VECTOR(4 DOWNTO 0);
+
+    SIGNAL ResultReady             	: STD_LOGIC;
+    SIGNAL LoopingAndResultNotReady     : STD_LOGIC;
 
 BEGIN
+    -------------------------------------------------------
+    -- Accelerator Counter
+    CounterCLK <= CLK AND NOT(ResultReady) AND NOT(Instr);
+    CounterRST <= Start OR RST;
+    ResultReady <= (CounterOut(0) AND NOT(CounterOut(1)) AND NOT(CounterOut(1)) AND CounterOut(3));
+    LoopingAndResultNotReady <= NOT(ResultReady) AND (CounterOut(0) OR CounterOut(1) OR CounterOut(2) OR CounterOut(3));
+
+    ACCELERATOR_COUNTER:
+    ENTITY work.counter
+    GENERIC MAP(n => 4)
+    PORT MAP(CLK => CounterCLK, RST => CounterRST, Dout => CounterOut);
+
 
 END ARCHITECTURE;
---
+++ components/counter/counter.vhd
@@ -0,0 +1,25 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
+
+ENTITY counter IS
+    GENERIC(n : INTEGER := 4);
+    PORT(
+        CLK     : IN  STD_LOGIC;
+        RST     : IN STD_LOGIC;
+        Dout    : INOUT STD_LOGIC_VECTOR(n-1 DOWNTO 0)
+    );
+END ENTITY;
+
+ARCHITECTURE arch_counter OF counter IS
+BEGIN
+
+    PROCESS(CLK, RST)
+    BEGIN
+        IF RST='1' THEN
+            Dout <= (OTHERS => '0');
+        ELSIF CLK='1' THEN
+            Dout <= STD_LOGIC_VECTOR(TO_UNSIGNED(TO_INTEGER(UNSIGNED(Dout)) + 1, n));
+        END IF;
+    END PROCESS;
+END ARCHITECTURE;
